9cd991381442245fd1438bc9cd69a0e2ecfaf96d
kostvick <my.mail.kostya@gmail.com>
2022-10-08 16:42:06 +0300

    change values

--- lib/models/playlist.dart
+++ lib/models/playlist.dart
@@ -1,14 +1,11 @@
-import 'package:parse_playlist/models/song.dart';
-
 class Playlist {
   final String title;
   final String playlistImage;
   final String description;
-  final List<Song> songs;
-  Playlist({
-    required this.title,
-    required this.playlistImage,
-    required this.description,
-    required this.songs,
-  });
+  final List<String> songs;
+  Playlist(
+      {required this.title,
+      required this.playlistImage,
+      required this.description,
+      required this.songs});
 }
--- lib/models/song.dart
+++ lib/models/song.dart
@@ -1,6 +1,4 @@
-import 'package:html/dom.dart';
 import 'package:parse_playlist/models/album.dart';
-import 'package:parse_playlist/utils/playlist_parser.dart';
 
 class Song {
   final String name;
@@ -14,18 +12,4 @@ class Song {
     required this.album,
     required this.duration,
   });
-
-  // factory Song.fromDocument(Document doc) {
-  //   final songTitle = PlaylistParser.findContentByMetaProperty(
-  //       doc: doc, property: 'og:title');
-  //   final artist = PlaylistParser.findContentByMetaProperty(
-  //           doc: doc, property: 'og:description')
-  //       .replaceAll(RegExp(r' · Song · \d+'), '');
-  //   final duration =
-  //       PlaylistParser.findContentByMetaName(doc: doc, name: 'music:duration');
-  //   final albumURL =
-  //       PlaylistParser.findContentByMetaName(doc: doc, name: 'music:album');
-  //   return Song(
-  //       name: songTitle, artist: artist, album: albumURL, duration: duration);
-  // }
 }
